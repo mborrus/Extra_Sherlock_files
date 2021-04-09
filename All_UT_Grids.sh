e40e3345d89545f13fb6a624ba7f9dd607bea567
Marshall <38109796+mborrus@users.noreply.github.com>
2021-04-09 14:18:53 -0400

    Update All_UT_Grids.sh

--- All_UT_Grids.sh
+++ All_UT_Grids.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #
 #SBATCH --array=1-20%5
-#SBATCH --output=./jobfiles/regridder_%j.out
-#SBATCH --error=./jobfiles/regridder_%j.err
+#SBATCH --error=./jobfiles/regridder_%A-%a.err
+#SBATCH --output=./jobfiles/regridder_%A-%a.out
 #SBATCH --job-name=regridder_UT
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=16G
 #SBATCH --time=00:30:00
 #SBATCH --verbose
+#SBTACH -p serc
+
+
+echo starting work on ${SLURM_ARRAY_TASK_ID}
 
 datapath="/scratch/users/mborrus/Globus_data/gfdl.intel18-prod-openmp-extra"
 cd $datapath/${SLURM_ARRAY_TASK_ID}
@@ -16,3 +20,10 @@ mv -v ./FREgrid/* .
 rm -rf FREgrid/
 chmod 755 GridRunner
 ./GridRunner
+
+echo finished regridding ${SLURM_ARRAY_TASK_ID}
+
+mk dir /oak/stanford/schools/ees/aditis2/Globus_data/gfdl.intel18-prod-4xCO2/${SLURM_ARRAY_TASK_ID}
+mv dailyUT.nc /oak/stanford/schools/ees/aditis2/Globus_data/gfdl.intel18-prod-4xCO2/${SLURM_ARRAY_TASK_ID}
+
+echo finished moving 
