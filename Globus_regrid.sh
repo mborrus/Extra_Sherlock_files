3f32ae5d18e9787e608ff994fa40e821509423ef
Marshall <38109796+mborrus@users.noreply.github.com>
2021-10-25 13:14:40 -0700

    Update Globus_regrid.sh

--- Globus_regrid.sh
+++ Globus_regrid.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 #
-#SBATCH --array=1-20%2
 #SBATCH --error=./jobfiles/regridder_%A-%a.err
 #SBATCH --output=./jobfiles/regridder_%A-%a.out
 #SBATCH --job-name=regridder_UT
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=8G
-#SBATCH --time=00:50:00
+#SBATCH --time=03:00:00
 #SBATCH --verbose
 #SBATCH -p serc
 
-
-echo starting work on ${SLURM_ARRAY_TASK_ID}
-
-datapath="/scratch/users/mborrus/Globus_data/gfdl.intel18-prod-4xCO2"
-cd $datapath/${SLURM_ARRAY_TASK_ID}
+datapath="/scratch/users/mborrus/gfdl.ncrc4-intel-prod-openmp-3aditi/2x3m0d_432x2a/history/"
+cd $datapath
 git clone https://github.com/mborrus/FREgrid.git
 mv -v ./FREgrid/* .
 rm -rf FREgrid/
 chmod 755 GridRunner_sherlock
-./GridRunner_sherlock
-
-echo finished regridding ${SLURM_ARRAY_TASK_ID}
-
-mv dailyUTP.nc /scratch/users/mborrus/AM4/Base/${SLURM_ARRAY_TASK_ID}/dailyUTP.nc
-echo finished moving 
+fregrid --input_mosaic C96_mosaic.nc --input_file 19790101.atmos_daily.nc --output_file dailyUTPS_1.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
+fregrid --input_mosaic C96_mosaic.nc --input_file 19790401.atmos_daily.nc --output_file dailyUTPS_2.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
