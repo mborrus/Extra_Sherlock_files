185222db00750101bbabed9db6c8e140ba8746c1
Marshall <38109796+mborrus@users.noreply.github.com>
2021-10-26 13:34:01 -0700

    Update Globus_regrid.sh

--- Globus_regrid.sh
+++ Globus_regrid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+#SBATCH --array=1-4
 #SBATCH --error=./jobfiles/regridder_%A-%a.err
 #SBATCH --output=./jobfiles/regridder_%A-%a.out
 #SBATCH --job-name=regridder_UT
@@ -9,29 +10,34 @@
 #SBATCH --verbose
 #SBATCH -p serc
 
-datapath="/scratch/users/mborrus/gfdl.ncrc4-intel-prod-openmp-3aditi/2x3m0d_432x2a/history/"
-cd $datapath
-git clone https://github.com/mborrus/FREgrid.git
-mv -v ./FREgrid/* .
-rm -rf FREgrid/
-chmod 755 GridRunner_sherlock
-fregrid --input_mosaic C96_mosaic.nc --input_file 19790101.atmos_daily.nc --output_file dailyUTPS_1.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
-fregrid --input_mosaic C96_mosaic.nc --input_file 19790401.atmos_daily.nc --output_file dailyUTPS_2.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
+ending=('' '1' '2' '3')
+index=${ending[${SLURM_ARRAY_TASK_ID}-1]}
 
-datapath="/scratch/users/mborrus/gfdl.ncrc4-intel-prod-openmp-3aditi/2x3m0d_432x2a1/history/"
-cd $datapath
-git clone https://github.com/mborrus/FREgrid.git
-mv -v ./FREgrid/* .
-rm -rf FREgrid/
-chmod 755 GridRunner_sherlock
-fregrid --input_mosaic C96_mosaic.nc --input_file 19790101.atmos_daily.nc --output_file dailyUTPS_1.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
-fregrid --input_mosaic C96_mosaic.nc --input_file 19790401.atmos_daily.nc --output_file dailyUTPS_2.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
+datapath="/scratch/users/mborrus/Globus_data/"
 
-datapath="/scratch/users/mborrus/gfdl.ncrc4-intel-prod-openmp-3aditi/2x3m0d_432x2a2/history/"
-cd $datapath
+cd $datapath/2x12m0d_432x2a${index}/history
 git clone https://github.com/mborrus/FREgrid.git
 mv -v ./FREgrid/* .
 rm -rf FREgrid/
 chmod 755 GridRunner_sherlock
-fregrid --input_mosaic C96_mosaic.nc --input_file 19790101.atmos_daily.nc --output_file dailyUTPS_1.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
-fregrid --input_mosaic C96_mosaic.nc --input_file 19790401.atmos_daily.nc --output_file dailyUTPS_2.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
+fregrid --input_mosaic C96_mosaic.nc --input_file 19800101.atmos_daily.nc --output_file dailyUTP.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
+mkdir /scratch/users/mborrus/AM4/TOM/Q1/${SLURM_ARRAY_TASK_ID}
+cp dailyUTP.nc /scratch/users/mborrus/AM4/TOM/Q1/${SLURM_ARRAY_TASK_ID}/dailyUTP.nc
+
+cd $datapath/2x6m0d_432x2a${index}/history
+git clone https://github.com/mborrus/FREgrid.git
+mv -v ./FREgrid/* .
+rm -rf FREgrid/
+chmod 755 GridRunner_sherlock
+fregrid --input_mosaic C96_mosaic.nc --input_file 19790701.atmos_daily.nc --output_file dailyUTP.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
+mkdir /scratch/users/mborrus/AM4/TOM/Q3/${SLURM_ARRAY_TASK_ID}
+cp dailyUTP.nc /scratch/users/mborrus/AM4/TOM/Q3/${SLURM_ARRAY_TASK_ID}
+
+cd $datapath/2x9m0d_432x2a${index}/history
+git clone https://github.com/mborrus/FREgrid.git
+mv -v ./FREgrid/* .
+rm -rf FREgrid/
+chmod 755 GridRunner_sherlock
+fregrid --input_mosaic C96_mosaic.nc --input_file 19791001.atmos_daily.nc --output_file dailyUTPS.nc --scalar_field ucomp,temp,precip,sphum --nthreads 6 --nlon 144 --nlat 90 --debug
+mkdir /scratch/users/mborrus/AM4/TOM/Q4/${SLURM_ARRAY_TASK_ID}
+cp dailyUTP.nc /scratch/users/mborrus/AM4/TOM/Q4/${SLURM_ARRAY_TASK_ID}
